8330a71bc8b6007889a812ef2a76cea39ab1d337
Jayden S <71941889+JaydenS115@users.noreply.github.com>
2021-10-31 17:40:03 -0400

    closeAndMoveFile()  function
    Implemented a helper function that can be used when File I/O operations go poorly.
    Argument: The file object with the opened file
    Body: Closes the file, stores its contents, and writes those contents to a file with [OLD] in name. If the file is the service Information file for the program, delete the file (so that it can be automatically re-made the next time the program is executed).

--- main.py
+++ main.py
@@ -5,13 +5,14 @@
 
 #imports
 import os
+from io import TextIOWrapper
 import getpass
 import hashlib
 
 
 # Program Opener and Information display
 PROGRAM_NAME = "Hashword"
-VERSION = "0.1.0"
+VERSION = "0.1.4"
 DESCRIPTION = "Password Generator and Handler"
 print("\n\n\t\t" + PROGRAM_NAME + " \t[v" + VERSION + "]\n\n\t\t" + DESCRIPTION + "\n\n")
 
@@ -20,17 +21,70 @@ print("\n\n\t\t" + PROGRAM_NAME + " \t[v" + VERSION + "]\n\n\t\t" + DESCRIPTION 
 SERVICE_INFO_FILE_PATH = "ServiceData.dat"  # the desired and checked-for name of service data file
 
 
-# Try to open service data file (check if existent in current directory)
+#
+# Function: closeAndMoveFile(fileObject)
+# If something "wrong" is found with a service info file,
+# invoke this function on the fileObject that has the file .open(),
+# and this function will close the file, move its contents elsewhere,
+# then delete the file, to be made new next execution of the program.
 
+def closeAndMoveFile(fileObject: TextIOWrapper):
+    
+    # hold onto file name for later re-opening
+    filePath = fileObject.name
+
+    # close file, then re-open in read mode
+    fileObject.close()
+    fileObject = open(filePath, "rt")
+
+    # get file's contents
+    fileContents = fileObject.read()
+    fileObject.close()
+
+    #
+    # get desired filename for service info file
+    svcFileInfo = filePath.split('.', 1)
+
+
+    svcFileName = svcFileInfo[0]
+    svcFileName = "[OLD]-"
+
+    svcFileExt = "."
+    svcFileExt += svcFileInfo[1]
+
+
+    #
+    # find next available [OLD] filename to store to
+    i = 1
+    while(os.path.exists(svcFileName + str(i) + svcFileExt)):
+        i += 1
+
+    #
+    # create and write to new file to store previous file contents
+    fileObject = open(svcFileName + str(i) + svcFileExt, "wt")
+    fileObject.write(fileContents)
+    # close file when done writing
+    fileObject.flush()
+    fileObject.close()
+
+    #
+    # if the previous file is SERVICE_INFO_FILE_PATH, delete it
+    # so that it can be re-made in the next execution of the program
+    if(filePath == SERVICE_INFO_FILE_PATH):
+        os.remove(filePath)
+
+
+# END FUNCTION: closeAndMoveFile(fileObject)
+
+
+
+#
+# Try to open service data file (check if existent in current directory)
 if not os.path.exists(SERVICE_INFO_FILE_PATH):
 
     #
-    #if NOT opened: create new file of that name
-
+    #if NOT opened: start to create new file of that name
     print("Service Information File \"" + SERVICE_INFO_FILE_PATH + "\" not found.\n\tPlease continue to create a new file.")
-
-    serviceDataFile = open(SERVICE_INFO_FILE_PATH, "wt")
-
 
     #
     # Ask user for hash algorithm to use (default = SHA256)
@@ -58,7 +112,15 @@ if not os.path.exists(SERVICE_INFO_FILE_PATH):
     hash.update(masterPasswordPlaintext)
     hashedMasterPassword = hash.hexdigest()
 
+    # securely clean up the plaintext password
+    # to remove it from memory and then from accidental use
+    masterPasswordPlaintext = ""
+    del masterPasswordPlaintext
+
     #
+    # create the file for writing
+    serviceDataFile = open(SERVICE_INFO_FILE_PATH, "wt")
+
     # store the hash algo used to the new file
     serviceDataFile.write("hash:" + hashAlgorithm + '\n')
 
@@ -74,8 +136,20 @@ if not os.path.exists(SERVICE_INFO_FILE_PATH):
 
 
 
+#
 # load info from the file into program
 serviceDataFile = open(SERVICE_INFO_FILE_PATH, "rt")
+
+#
+# Read from file and store for use in program
+# Info is stored in this order: hash algorithm, master password's hash, 
+#   then all of the services and their maximum password lengths (to truncate the password to)
+
+
+#hashAlgorithm = 
+#hashedMasterPassword = 
+#serviceDictionary = 
+
 
 
         # Ask the user for master password
@@ -95,6 +169,7 @@ serviceDataFile = open(SERVICE_INFO_FILE_PATH, "rt")
 
 
     # if service NOT found in Data File:
+    
 
         # Notify the user that they need to register the new service
         # Ask the user for the max length allowed for the password
@@ -102,6 +177,9 @@ serviceDataFile = open(SERVICE_INFO_FILE_PATH, "rt")
         # Enter this service into the data file w/ the information
 
     
+
+#close service info file (done with it for this execution)
+serviceDataFile.close()
 
 
 # Take the selected service's name and append it to end of master password given earlier
