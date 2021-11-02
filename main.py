abc33ee66783f66e20e21f6ef27c13b732b3ee59
Jayden S <71941889+JaydenS115@users.noreply.github.com>
2021-11-02 17:34:15 -0400

    Changed to pyperclip module
    Change clipboard management to pyperclip module instead of tkinter, as it seems to have abug that temporarily crashes the pasted-to program.
    Use 'pip install pyperclip' to install module locally for use in the program.

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ import os
 from io import TextIOWrapper
 import getpass
 import hashlib
-from tkinter import Tk
 from time import sleep
+import pyperclip
 
 
 # Program Opener and Information display
@@ -77,48 +77,6 @@ def closeAndMoveFile(fileObject: TextIOWrapper):
 
 
 # END FUNCTION: closeAndMoveFile(fileObject)
-
-
-#
-# Clipboard management functions, for use at end of program
-#
-def copyToClipboard(content):
-    TkinterTk = Tk()
-    TkinterTk.withdraw()
-
-    # clear, then add to clipboard
-    TkinterTk.clipboard_clear()
-    TkinterTk.clipboard_append(content)
-
-    #TkinterTk.update()
-    TkinterTk.destroy()
-
-
-# Read (view) clipboard contents
-def getClipboardContents():
-    TkinterTk = Tk()
-    TkinterTk.withdraw()
-
-    # store clipboard contents
-    content = TkinterTk.clipboard_get()
-    TkinterTk.destroy()
-
-    return content
-
-
-# Clear (remove) clipboard contents
-def clearClipboard():
-    TkinterTk = Tk()
-    TkinterTk.withdraw()
-
-    # clear clipboard contents
-    TkinterTk.clipboard_clear()
-
-    #TkinterTk.update()
-    TkinterTk.destroy()
-
-
-# END FUNCTIONS: clipboard management
 
 
 
@@ -192,43 +150,54 @@ serviceDataFile = open(SERVICE_INFO_FILE_PATH, "rt")
 
 #hashAlgorithm = 
 #hashedMasterPassword = 
-#serviceDictionary = 
+serviceDictionary = {}
+
+
+#close service info file (as we're done with it for this execution)
 
 
 
-        # Ask the user for master password
+# Ask the user for master password
 
 
-        # Hash the master password
+# Hash the master password
 
 
-        # compare Hash to the hash stored in password data file
+# compare Hash to the hash stored in password data file
 
-            # if NOT matching: ask for re-try UNTIL successful hash-match
+    # if NOT matching: ask for re-try UNTIL successful hash-match
 
 
 
 #
 # Ask the user which service they want their password for
-serviceName = (input("\nEnter Service Name: ") or '')
+serviceName = input("\nEnter Service Name: ")
 
-while(serviceName == ''):
+while(serviceName == ''): # re-try until you get an input
     print("\n\tPlease enter the name of the service you would like to retrieve your password for.")
     serviceName = (input("\nEnter Service Name: ") or '')
 
 
-    # if service NOT found in Data File:
-    
+# if service NOT found in Data File:
+if(serviceName not in serviceDictionary):
 
-        # Notify the user that they need to register the new service
-        # Ask the user for the max length allowed for the password
+    # Notify the user that they need to register the new service
+    print("\n\tService \"" + serviceName + "\" not registered.\n\t\tBeginning Registration.")
+
+    # Ask the user for the max length allowed for the password
+    maxPassLength = input("\n\tEnter Maximum " + serviceName + " Password Length: ")
+
+    while(str(maxPassLength) == ''): # re-try until you get an input
+        print("\n\tPlease enter the maximum length for a " + serviceName + " password.")
+        maxPassLength = input("\n\tEnter Maximum " + serviceName + " Password Length: ")
         
-        # Enter this service into the data file w/ the information
+    #
+    # Append this service into the data file w/ the information
+    serviceDataFile = open(SERVICE_INFO_FILE_PATH, "at")
+    serviceDataFile.write(serviceName + ':' + str(maxPassLength) + '\n')
+    serviceDataFile.close()
 
-    
 
-#close service info file (done with it for this execution)
-serviceDataFile.close()
 
 
 # Take the selected service's name and append it to end of master password given earlier
@@ -241,19 +210,20 @@ serviceDataFile.close()
 
 
 # Copy the password to the user's clipboard
-copyToClipboard(hashedMasterPassword)
+pyperclip.copy(hashedMasterPassword)
 
-print("\n\t\tPassword for \"" + serviceName + "\" copied to clipboard.\n")
+print("\n\t\tCopied Password for \"" + serviceName + "\" to Clipboard.\n")
 
 
 # Wait a few seconds, then clear the user's clipboard to clear the password data
 sleep(10)
 
-if(getClipboardContents() == hashedMasterPassword):
-    clearClipboard()
+if(pyperclip.paste() == hashedMasterPassword):
+    pyperclip.copy("")
 
 
-# clean memory for security
+#
+# cleanup values in memory for security purposes
 hashedMasterPassword = ''
 del hashedMasterPassword
 
